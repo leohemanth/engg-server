e79d8d9e3889f186006d8b8d1545f6b152df1619
Hemanth Prasad <leohemanth@Hemanths-MacBook-Air.local>
2012-02-27 22:45:32 +0530

    first commit

---
+++ engg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Engg::Application.config.secret_token = '7b8591e31323b1feb3cf75dce0e673c7388bf1205fa49e1bd5acb4a384b63683e6e794880ceee8d00aada0ee5de0b1b9e2ddb330b1391fb179173115d373c90d'
